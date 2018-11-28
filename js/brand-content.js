516b3cb6f04038f8e0dbed98591d26002848faa3
YWang <2784484262@qq.com>
2018-11-28 11:20:38 +0800

    商品排行

---
+++ js/brand-content.js
@@ -0,0 +1,63 @@
+$(function () {
+  var m = new MMBBrand();
+  m.tab('.tab-wrap', '.content').tpl({
+    url: 'api/getbrand',
+    data: {
+      brandtitleid: 0
+    },
+    tpl: 'brandTop10tpl',
+    location: '#brandTop'
+
+  }).tpl({
+    url: 'api/getbrandproductlist',
+    data: {
+      brandtitleid: 0
+    },
+    tpl: 'salesTop10tpl',
+    location: '#salesTop'
+
+  }).tpl({
+    url: 'api/getproductcom',
+    data: {
+      productid: 1
+    },
+    tpl: 'pingLunTpl',
+    location: '#pingLun'
+
+  })
+
+})
+
+function MMBBrand() {
+  this.v = '1.0';
+}
+
+MMBBrand.prototype = {
+  constructor: MMBBrand,
+  config: {
+    urlHead: 'http://localhost:9090/'
+  },
+  tab: function (tabhead, tabcontent) {
+    $(tabhead).find('[data-title]').on('click', function (e) {
+      $(this).addClass('mui-active').siblings().removeClass('mui-active');
+      $(tabcontent).find("[data-content=" + $(this).data('title') + "]").addClass('mui-active').siblings().removeClass('mui-active');
+    })
+    return this;
+  },
+
+  tpl: function (data) {
+
+    var that = this;
+    $.ajax({
+      type: 'get',
+      url: that.config.urlHead + data.url,
+      data: data.data,
+      dataType: 'json',
+      success: function (response) {
+        var html = template(data.tpl, response);
+        $(data.location).html(html);
+      }
+    })
+    return that;
+  }
+}
